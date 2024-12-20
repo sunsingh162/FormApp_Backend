f764bba4bcfef0a4d919bba11b0d1514d202d57b
Sunny Singh <sunnysingh@Sunnys-MacBook-Air.local>
2024-12-20 18:34:09 +0530

    Validate user password and created login and logout api

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -18,7 +18,7 @@ authRouter.post("/signup", async (req, res) => {
     const user = new UserModel({
       userName,
       email,
-      password: hashPassword
+      password: hashPassword,
     });
 
     const savedUser = await user.save();
@@ -31,6 +31,40 @@ authRouter.post("/signup", async (req, res) => {
   } catch (err) {
     res.status(400).send("ERROR : " + err.message);
   }
+});
+
+//Login
+authRouter.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await UserModel.findOne({ email: email });
+
+    if (!user) {
+      throw new Error("Invalid credentials");
+    }
+
+    const isPasswordValid = await user.validatePassword(password);
+
+    if (isPasswordValid) {
+      // Create a JWT Token
+      const token = await user.getJWT();
+      res.cookie("token", token, {
+        expires: new Date(Date.now() + 8 * 3600000),
+      });
+      res.json({ message: "Login successful", data: user });
+    } else {
+      throw new Error("Invalid Credentials");
+    }
+  } catch (err) {
+    res.status(400).send("ERROR : " + err.message);
+  }
+});
+
+//Logout user
+authRouter.post("/logout", (req, res) => {
+  res.cookie("token", null, { expires: new Date(Date.now()) });
+  res.send("Logout Successfully");
 });
 
 module.exports = authRouter;
